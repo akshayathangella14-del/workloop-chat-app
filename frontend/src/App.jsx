0c76956d2810f356b16cdfeb1a8d2cd1968b1882
unknown <akshayathangella14@gmail.com>
2026-05-05 20:30:14 +0530

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+function App() {
+  return (
+    <div className="h-screen bg-slate-900 flex items-center justify-center">
+      <div className="bg-white p-10 rounded-2xl shadow-2xl text-center">
+        <h1 className="text-3xl font-bold text-gray-800 mb-4">
+          Tailwind is Working 🎉
+        </h1>
+
+        <p className="text-gray-600 mb-6">
+          Your Slack Clone Frontend Setup is Ready
+        </p>
+
+        <button className="bg-purple-600 hover:bg-purple-700 text-white px-6 py-2 rounded-lg">
+          Start Building UI
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default App
